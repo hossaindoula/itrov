269b22d2adcf270f69665134474d0603f58fbed1
sahil.abrar@gmail.com <sahil.abrar@gmail.com@7d397268-1e35-030d-f0ed-38381cf212f0>
2013-05-02 12:50:36 +0000

    FrontEndController.java updated..

--- trunk/ITRovers/source/com/itrovers/controller/FrontEndController.java
+++ trunk/ITRovers/source/com/itrovers/controller/FrontEndController.java
@@ -23,9 +23,16 @@ public class FrontEndController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView ourStory(){
-        Map<String,Object> ourStoryModel = new HashMap<String, Object>();
+        Map<String,Object> our_storyModel = new HashMap<String, Object>();
 
-        return new ModelAndView("/our_story", ourStoryModel);
+        return new ModelAndView("/our_story", our_storyModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView our_work(){
+        Map<String,Object> our_workModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/our_work", our_workModel);
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -36,10 +43,10 @@ public class FrontEndController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView contactUs(){
-        Map<String,Object> contactUsModel = new HashMap<String, Object>();
+    public ModelAndView contact_us(){
+        Map<String,Object> contact_usModel = new HashMap<String, Object>();
 
-        return new ModelAndView("/contact_us", contactUsModel);
+        return new ModelAndView("/contact_us", contact_usModel);
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -55,4 +62,99 @@ public class FrontEndController {
 
         return new ModelAndView("/locations", locationsModel);
     }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView approach(){
+        Map<String,Object> approachModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/approach", approachModel);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView awards(){
+        Map<String,Object> awardsModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/awards", awardsModel);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView careers(){
+        Map<String,Object> careersModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/careers", careersModel);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView executive_team(){
+        Map<String,Object> executive_teamModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/executive_team", executive_teamModel);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView industry_solutions(){
+        Map<String,Object> industry_solutionsModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/industry_solutions", industry_solutionsModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView integrated_marketing(){
+        Map<String,Object> integrated_marketingModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/integrated_marketing", integrated_marketingModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView interactive_services(){
+        Map<String,Object> interactive_servicesModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/interactive_services", interactive_servicesModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView management_team(){
+        Map<String,Object> management_teamModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/management_team", management_teamModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView marketing_innovation(){
+        Map<String,Object> marketing_innovationModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/marketing_innovation", marketing_innovationModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView news(){
+        Map<String,Object> newsModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/news", newsModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView people(){
+        Map<String,Object> peopleModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/people", peopleModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView product_solutions(){
+        Map<String,Object> product_solutionsModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/product_solutions", product_solutionsModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView web_hosting(){
+        Map<String,Object> web_hostingModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/web_hosting", web_hostingModel);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView welcome(){
+        Map<String,Object> welcomeModel = new HashMap<String, Object>();
+
+        return new ModelAndView("/welcome", welcomeModel);
+    }
+
 }
