dd6cf3a49660b52ae60e29a2f277db51af18b691
sahil.abrar@gmail.com <sahil.abrar@gmail.com@7d397268-1e35-030d-f0ed-38381cf212f0>
2013-05-08 19:47:47 +0000

    aop , controller and dao layer updated..

--- trunk/ITRovers/source/com/itrovers/dao/SecurityDaoImpl.java
+++ trunk/ITRovers/source/com/itrovers/dao/SecurityDaoImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+@Repository
 public class SecurityDaoImpl implements SecurityDao {
 
     @Autowired
